627600c319df117b1873659e53772971c355853f
David Sanchez <davidmsanchez250@gmail.com>
2021-05-19 22:31:36 -0400

    added foreign key to belongToMany relationships. Completed router.get for product-routes.js.

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -7,7 +7,9 @@ const { Product, Category, Tag, ProductTag } = require('../../models');
 router.get('/', (req, res) => {
   try {
     const productData = await Product.findAll({
-      include: [{model: Category, through: Tag,}]
+      include: [
+        Category,
+        {model: Tag, through: ProductTag}]
     });
     res.status(200).json(productData)
   } catch (err){
@@ -19,6 +21,23 @@ router.get('/', (req, res) => {
 
 // get one product
 router.get('/:id', (req, res) => {
+  try {
+    const productData = await Product.findByPk(req.params.id, {
+      include: [
+        Category,
+        {model: Tag, through: ProductTag}]
+    });
+
+    if (!productData) {
+      res.status(404).json({ message: 'This specific product was not found'});
+      return;
+    }
+
+    res.status(200).json(productData);
+
+  } catch (error) {
+    res.status(500).json(err)
+  }
   // find a single product by its `id`
   // be sure to include its associated Category and Tag data
 });
